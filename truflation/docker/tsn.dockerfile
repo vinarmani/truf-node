a7044e5aecf8ded3a09fb4c9dc651987ba786b1a
Raffael Campos <raffael.rcampos@gmail.com>
2024-03-08 10:54:30 -0300

    Update Docker configuration and setup script
    A new 'config.toml' file has been added to allow external access to the application's HTTP server. Updates to 'tsn.dockerfile' include copying the new 'config.toml' and setting up a new ENV variable 'PRIVATE_KEY'. Changes to 'setup.sh' script involve better process handling, error checks, and conditional config file creation. A new smoke test has also been added.

--- truflation/docker/tsn.dockerfile
+++ truflation/docker/tsn.dockerfile
@@ -21,6 +21,7 @@ RUN GIT_VERSION=$version GIT_COMMIT=$git_commit BUILD_TIME=$build_time CGO_ENABL
 RUN GIT_VERSION=$version GIT_COMMIT=$git_commit BUILD_TIME=$build_time CGO_ENABLED=0 TARGET="/app/.build" ./scripts/build/binary kwil-cli
 RUN chmod +x /app/.build/kwild /app/.build/kwil-admin /app/.build/kwil-cli
 
+# owner public address for this PK is 7e5f4552091a69125d5dfcb7b8c2659029395bdf
 ENV PRIVATE_KEY="0000000000000000000000000000000000000000000000000000000000000001"
 
 RUN /app/truflation/scripts/setup.sh
@@ -35,6 +36,7 @@ COPY --from=build /app/.build/kwil-admin ./kwil-admin
 
 # copy the produced data
 COPY --from=build /root/.kwild /root/.kwild
+COPY --from=build /app/truflation/docker/config.toml /root/.kwild/config.toml
 
 EXPOSE 50051 50151 8080 26656 26657
 ENTRYPOINT ["/app/kwild"]
--- truflation/scripts/setup.sh
+++ truflation/scripts/setup.sh
@@ -6,16 +6,27 @@ set -e
 cd "$(dirname "$0")"
 
 # run it in parallel, assign process pid to kwild_pid
-../../.build/kwild --autogen & kwild_pid=$!
+../../.build/kwild --autogen &> /dev/null & kwild_pid=$!
+
+function cleanup {
+  echo "Killing kwild process $kwild_pid"
+  kill -9 $kwild_pid
+}
+
+# trap the cleanup function to SIGINT, SIGTERM and EXIT signals
+trap cleanup SIGINT SIGTERM EXIT
 
 # to make sure kwild is ready
 sleep 5;
 
-mkdir -p ~/.kwil_cli
-echo "{\"private_key\":\"$PRIVATE_KEY\",\"grpc_url\":\"http://localhost:8080\",\"chain_id\":\"\"}" > ~/.kwil_cli/config.json
+
+# if $PRIVATE_KEY is setup and config does not exist, we create with
+if [ -n "$PRIVATE_KEY" ] && [ ! -f ~/.kwil_cli/config.json ]; then
+  mkdir -p ~/.kwil_cli
+  echo "{\"private_key\":\"$PRIVATE_KEY\",\"grpc_url\":\"http://localhost:8080\",\"chain_id\":\"\"}" > ~/.kwil_cli/config.json
+fi
 
 # smoke test about kwil-cli
-echo "SMOKE TEST"
 test_content=$(../../.build/kwil-cli database list --self)
 
 # if contains Error, error out
@@ -38,5 +49,14 @@ python ./produce_source_maps/process_all.py
 ./database_deploy.sh --skip-drop
 ./database_add_primitives.sh
 
-echo "Killing kwild process $kwild_pid"
-kill -9 $kwild_pid
+sleep 10;
+
+# smoke test
+output=$(../../.build/kwil-cli database call -a=get_index date:"2023-01-01" date_to:"2023-12-31" -n=com_numbeo_us_bread_3m_avg)
+# output should contain `2023-12-01` to be sure it worked
+if [[ $output != *"2023-12-01"* ]]; then
+  echo "Smoke test failed: $output"
+  exit 1
+fi
+
+echo "Success!"
