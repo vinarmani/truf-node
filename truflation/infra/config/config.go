a24b3cb1a0ff2097aa46b1c83a14a512f5724613
Raffael Campos <raffael.rcampos@gmail.com>
2024-03-13 05:56:28 -0300

    feat: provide a staging server (#86)
    * initial commit of cdk implementation
    
    * Update instance creation and deployment setup in CDK
    
    This commit streamlines creation and configuration of instances using the updated docker image. The image name will now get appended with imageAsset hash to ensure updates when image changes. The instance size has been updated from NANO to SMALL for better performance. Furthermore, Docker installation and service steps have been added to initialize scripts to automate the setup. Deployment stage in the CDK setup has been updated from 'DEV' to 'STAGING'.
    
    * Renamed 'cdk' directory to 'infra' and updated imports
    
    The name of the directory 'cdk' has been changed to 'infra'. Accordingly, all the import paths and related configurations that previously pointed to 'cdk' have been updated to now point to 'infra'. This change is reflected across multiple files, including .go, .json and .mod files.
    
    * add infra to go work example

---
+++ truflation/infra/config/config.go
@@ -0,0 +1,63 @@
+package config
+
+import (
+	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
+)
+
+// DO NOT modify this function, change stack name by 'cdk.json/context/stackName'.
+func StackName(scope constructs.Construct) string {
+	stackName := "MyEKSClusterStack"
+
+	ctxValue := scope.Node().TryGetContext(jsii.String("stackName"))
+	if v, ok := ctxValue.(string); ok {
+		stackName = v
+	}
+
+	return stackName
+}
+
+// DO NOT modify this function, change EC2 key pair name by 'cdk.json/context/keyPairName'.
+func KeyPairName(scope constructs.Construct) string {
+	keyPairName := "MyKeyPair"
+
+	ctxValue := scope.Node().TryGetContext(jsii.String("keyPairName"))
+	if v, ok := ctxValue.(string); ok {
+		keyPairName = v
+	}
+
+	return keyPairName
+}
+
+// DO NOT modify this function, change ECR repository name by 'cdk.json/context/imageRepoName'.
+func EcrRepoName(scope constructs.Construct) string {
+	ecrRepoName := "MyRepository"
+
+	ctxValue := scope.Node().TryGetContext(jsii.String("imageRepoName"))
+	if v, ok := ctxValue.(string); ok {
+		ecrRepoName = v
+	}
+
+	return ecrRepoName
+}
+
+// Deployment stage config
+type DeploymentStageType string
+
+const (
+	DeploymentStage_DEV     DeploymentStageType = "DEV"
+	DeploymentStage_STAGING DeploymentStageType = "STAGING"
+	DeploymentStage_PROD    DeploymentStageType = "PROD"
+)
+
+// DO NOT modify this function, change EKS cluster name by 'cdk-cli-wrapper-dev.sh/--context deploymentStage='.
+func DeploymentStage(scope constructs.Construct) DeploymentStageType {
+	deploymentStage := DeploymentStage_PROD
+
+	ctxValue := scope.Node().TryGetContext(jsii.String("deploymentStage"))
+	if v, ok := ctxValue.(string); ok {
+		deploymentStage = DeploymentStageType(v)
+	}
+
+	return deploymentStage
+}
