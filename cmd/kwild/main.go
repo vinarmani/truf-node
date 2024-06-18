d50ad63ce9f9580ae0a3f83763de23bc13198500
Raffael Campos <raffael.rcampos@gmail.com>
2024-06-18 10:12:15 -0300

    feat: access to latest kwil features (#308)
    * Remove unused extensions from main.go
    
    The commit removes several extension imports from the main.go file of the kwilteam program. The removed extensions were not being used in the current version of the code, so this cleaning up makes it clearer and less cluttered. New extensions can be initialized in the specified area if needed in the future.
    
    * Update Dockerfile to improve build and setup process
    
    This commit updates the Dockerfile to include the download and execution of scripts for binary files and simplifies the copying process during build. It also streamlines the setup by copying all necessary tools from the busybox, moving .build content directly to the app directory, and adjusting exposed ports.
    
    * remove all unneeded internal files
    
    we don't use go extensions, then these files became obsolete
    
    * Refactor binary download process into a separate script
    
    The previous inline way of downloading and organizing binaries directly from Taskfile has been extracted into a dedicated shell script file. This new script not only streamlines the downloading process but also correctly handles different OS and architecture high-level differences, improving cross-platform compatibility.
    
    * Update package dependencies
    
    Several Go dependencies have been updated to their newer versions. These include, but are not limited to kwil components
    
    * Remove obsolete build scripts
    
    The obsolete files .git_variables, .go_variables, and binary have been removed from the scripts/build folder. These files are no longer needed and their removal helps to clean up the codebase.
    
    * delete legacy develop experiments directory
    
    * delete unused scripts
    
    * temporary skip of go tests

--- cmd/kwild/main.go
+++ cmd/kwild/main.go
@@ -4,14 +4,6 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/kwilteam/kwil-db/extensions/auth"
-	"github.com/kwilteam/kwil-db/extensions/precompiles"
-	"github.com/truflation/tsn-db/internal/extensions/composed_stream"
-	"github.com/truflation/tsn-db/internal/extensions/ed25519authenticator"
-	"github.com/truflation/tsn-db/internal/extensions/mathutil"
-	"github.com/truflation/tsn-db/internal/extensions/primitive_stream"
-	"github.com/truflation/tsn-db/internal/extensions/whitelist"
-
 	"github.com/kwilteam/kwil-db/cmd/kwild/root"
 )
 
@@ -24,28 +16,5 @@ func main() {
 }
 
 func init() {
-	err := auth.RegisterAuthenticator("ed25519_example", ed25519authenticator.Ed25519Authenticator{})
-	if err != nil {
-		panic(err)
-	}
-
-	err = precompiles.RegisterPrecompile("mathutil", mathutil.InitializeMathUtil)
-	if err != nil {
-		panic(err)
-	}
-
-	err = precompiles.RegisterPrecompile("composed_stream", composed_stream.InitializeComposedStream)
-	if err != nil {
-		panic(err)
-	}
-
-	err = precompiles.RegisterPrecompile("primitive_stream", primitive_stream.InitializePrimitiveStream)
-	if err != nil {
-		panic(err)
-	}
-
-	err = precompiles.RegisterPrecompile("whitelist", whitelist.InitializeExtension)
-	if err != nil {
-		panic(err)
-	}
+	// initialize extensions here if needed
 }
