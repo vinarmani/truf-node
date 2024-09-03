8843b2b6841f061787dac3dffa756923fbadfd0e
Raffael Campos <raffael.rcampos@gmail.com>
2024-09-02 21:29:51 -0300

    chore(contract): optimize `get_index_change` (#527)
    * Refactor: Improve schema setup and benchmark case handling
    
    Updated the benchmark setup to use tree structure for schemas and enhanced the benchmark case handling. Adjusted setup functions, created a `SetupSchemasInput`, modified results handling, and added more descriptive comments.
    
    * Increase state machine timeout and add comment in benchmark
    
        Extended the state machine timeout from 30 to 120 minutes to accommodate longer-running benchmarks. Added a comment in the runSingleTest function to clarify the query of the index-0 stream as the root stream.
    
    * Add depth check to prevent benchmarks from exceeding limits
    
    Introduce a check in the benchmark setup to ensure that the tree's maximum depth does not exceed the PostgreSQL limitations, preventing potential errors. Added a new constant, maxDepth, set at 179, based on empirical findings.
    
    * Increase timeouts and adjust polling intervals
    
    Extended the state machine timeout from 2 hours to 6 hours to accommodate longer processing times. Adjusted task-specific timeouts and added a new polling interval to optimize the frequency of status checks during prolonged operations.
    
    * Update execution timeout parameter in Step Functions
    
    Changed the "timeoutSeconds" parameter to "executionTimeout" for better clarity. Also corrected the naming convention of the "TimeoutSeconds" constant to align with the updated AWS guideline.
    
    * Optimize get_index_change to remove nested loop
    
    * Add index change tests for contract validation
    
    Implement test cases to validate index change and YoY index calculations. Includes initialization, data insertion, and result conversion to ensure accuracy and coverage of edge cases.

--- deployments/infra/stacks/benchmark/step_functions.go
+++ deployments/infra/stacks/benchmark/step_functions.go
@@ -104,7 +104,7 @@ func createStateMachine(scope constructs.Construct, input CreateStateMachineInpu
 
 	stateMachine := awsstepfunctions.NewStateMachine(scope, jsii.String("BenchmarkStateMachine"), &awsstepfunctions.StateMachineProps{
 		DefinitionBody: awsstepfunctions.DefinitionBody_FromChainable(benchmarkWorkflowChain),
-		Timeout:        awscdk.Duration_Minutes(jsii.Number(120)),
+		Timeout:        awscdk.Duration_Hours(jsii.Number(6)),
 		// <stackname>-benchmark
 		StateMachineName: jsii.String(fmt.Sprintf("%s-benchmark", *awscdk.Aws_STACK_NAME())),
 	})
