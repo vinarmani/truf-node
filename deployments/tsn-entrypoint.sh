5f543df5756c61f97568153958301e67668ab4fe
Michael Buntarman <michaelboentarman@gmail.com>
2024-09-18 18:59:43 +0700

    feat: sync new nodes with snapshot option (#608)

--- deployments/tsn-entrypoint.sh
+++ deployments/tsn-entrypoint.sh
@@ -8,5 +8,6 @@ exec /app/kwild --root-dir $CONFIG_PATH \
        --app.http-listen-addr "0.0.0.0:8080"\
        --app.jsonrpc-listen-addr "0.0.0.0:8484"\
        --app.db-read-timeout "60s"\
+       --app.snapshots.enabled\
        --chain.p2p.listen-addr "tcp://0.0.0.0:26656"\
        --chain.rpc.listen-addr "tcp://0.0.0.0:26657"
