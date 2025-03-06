6756dc1338011334943a98ea3434a9d4e78541dc
Michael Buntarman <michaelboentarman@gmail.com>
2025-03-06 17:10:45 +0700

    chore: add stream_ids into action parameter

--- deployments/infra/go.mod
+++ deployments/infra/go.mod
@@ -1,8 +1,8 @@
 module github.com/trufnetwork/node/infra
 
-go 1.22.7
+go 1.23.0
 
-toolchain go1.23.6
+toolchain go1.24.0
 
 require (
 	github.com/aws/aws-cdk-go/awscdk/v2 v2.146.0
@@ -24,7 +24,7 @@ require (
 	github.com/fbiville/markdown-table-formatter v0.3.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/exp v0.0.0-20250128182459-e0ece0dbea4c // indirect
+	golang.org/x/exp v0.0.0-20250218142911-aa4b98e5adaa // indirect
 	golang.org/x/sync v0.11.0 // indirect
 )
 
@@ -42,11 +42,11 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/yuin/goldmark v1.4.13 // indirect
-	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/crypto v0.35.0 // indirect
 	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
 	golang.org/x/mod v0.23.0 // indirect
-	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/net v0.35.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
-	golang.org/x/tools v0.29.0 // indirect
+	golang.org/x/tools v0.30.0 // indirect
 )
--- internal/migrations/002-primitive-insertion.sql
+++ internal/migrations/002-primitive-insertion.sql
@@ -3,7 +3,7 @@
     This file contains actions related to the primitive events stream.
 */
 
-CREATE ACTION insert_record(
+CREATE OR REPLACE ACTION insert_record(
     $stream_id TEXT,
     $ts INT8,
     $value NUMERIC(36,18)
@@ -16,8 +16,8 @@ CREATE ACTION insert_record(
         ERROR('wallet not allowed to write');
     }
 
-    -- Ensure that the stream/contract is initiated
-    if is_initiated() == false {
+    -- Ensure that the stream/contract is existent
+    if is_existent($stream_id) == false {
         ERROR('contract must be initiated');
     }
 
@@ -28,9 +28,10 @@ CREATE ACTION insert_record(
     VALUES ($stream_id, $data_provider, $ts, $value, $current_block);
 };
 
-CREATE ACTION is_initiated() PUBLIC view returns (result bool) {
-    -- Check if the contract/stream has been initiated by verifying metadata "type"
-    for $row in SELECT * FROM metadata WHERE metadata_key = 'type' LIMIT 1 {
+-- is_existent checks if the stream is existent
+CREATE OR REPLACE ACTION is_existent($data_provider TEXT, $stream_id TEXT) PUBLIC view returns (result bool) {
+    -- Check if the stream is initiated
+    for $row in SELECT * FROM metadata WHERE metadata_key = 'type' AND stream_id = $stream_id AND data_provider = $data_provider LIMIT 1 {
          return true;
     }
     return false;
@@ -38,7 +39,9 @@ CREATE ACTION is_initiated() PUBLIC view returns (result bool) {
 
 -- This action wraps metadata selection with pagination parameters.
 -- It supports ordering only by created_at ascending or descending.
-CREATE ACTION get_metadata(
+CREATE OR REPLACE ACTION get_metadata(
+    $data_provider TEXT,
+    $stream_id TEXT,
     $key TEXT,
     $only_latest BOOL,
     $ref TEXT,
@@ -72,17 +75,19 @@ CREATE ACTION get_metadata(
                   value_s,
                   value_ref,
                   created_at
-           FROM metadata
+        FROM metadata
            WHERE metadata_key = $key
-             AND disabled_at IS NULL
-             AND ($ref IS NULL OR LOWER(value_ref) = LOWER($ref))
-           ORDER BY
+            AND disabled_at IS NULL
+            AND ($ref IS NULL OR LOWER(value_ref) = LOWER($ref))
+            AND stream_id = $stream_id
+            AND data_provider = $data_provider
+       ORDER BY
                CASE WHEN $order_by = 'created_at DESC' THEN created_at END DESC,
                CASE WHEN $order_by = 'created_at ASC' THEN created_at END ASC
-           LIMIT $limit OFFSET $offset;
+       LIMIT $limit OFFSET $offset;
 };
 
-CREATE ACTION is_wallet_allowed_to_write(
+CREATE OR REPLACE ACTION is_wallet_allowed_to_write(
     $wallet TEXT,
     $limit INT,
     $offset INT,
